69baab37b3d34b14f58d6a5a4f3ecf267510ff65
robomex <atmorales@gmail.com>
2015-08-14 00:46:17 -0500

    move segmentedControl out of VenueDetailsVC and set bg to blue for testing

--- VenueDetailsViewController.swift
+++ VenueDetailsViewController.swift
@@ -12,24 +12,14 @@ class VenueDetailsViewController: UIViewController {
 
     @IBOutlet weak var venueDetailsLabel: UILabel!
     
-    var segmentedControl: UISegmentedControl!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        
-        let frame = UIScreen.mainScreen().bounds
-        segmentedControl = UISegmentedControl(items: ["Chat", "Details"])
-        segmentedControl.frame = CGRectMake(frame.minX + 100, frame.minY + 70, frame.width - 200, frame.height * 0.04)
-        segmentedControl.selectedSegmentIndex = 1
-        segmentedControl.addTarget(self, action: "venueSegmentedControlAction:", forControlEvents: .ValueChanged)
-        segmentedControl.backgroundColor = UIColor.whiteColor()
-        segmentedControl.tintColor = UIColor(red: 0x9b/255, green: 0x59/255, blue: 0xb6/255, alpha: 1.0)
-        self.view.addSubview(segmentedControl)
-    }
 
+        view.backgroundColor = UIColor.blueColor()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,15 +35,4 @@ class VenueDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-    func venueSegmentedControlAction(sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 1:
-            let vc = VenueDetailsViewController()
-            vc.title = "test"
-            navigationController?.pushViewController(vc, animated: false)
-        default:
-            return
-        }
-    }
 }
