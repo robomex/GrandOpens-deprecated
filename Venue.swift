2292e3388b51d7d4f4fcd4288be1f3d5fa0bb252
robomex <atmorales@gmail.com>
2015-08-24 23:33:55 -0500

    added openingDate to Venue Struct

--- Venue.swift
+++ Venue.swift
@@ -12,6 +12,7 @@ import Parse
 struct Venue {
     let id: String
     let name: String
+    let openingDate: NSDate
     let address: String
     let neighborhood: String
     let description: String
@@ -29,13 +30,13 @@ func fetchVenues (callback: ([Venue]) -> ()) {
             
             if let venues = objects as? [PFObject] {
                 let fetchedVenues = venues.map({
-                    (object: PFObject) -> (venueId: String, venueName: String, venueAddress: String, venueNeighborhood: String, venueDescription: String, venueFoodType: String) in
-                    (object.objectId! as String, object.objectForKey("name") as! String, object.objectForKey("address") as! String, object.objectForKey("neighborhood") as! String, object.objectForKey("description") as! String, object.objectForKey("foodType") as! String)
+                    (object: PFObject) -> (venueId: String, venueName: String, venueOpeningDate: NSDate, venueAddress: String, venueNeighborhood: String, venueDescription: String, venueFoodType: String) in
+                    (object.objectId! as String, object.objectForKey("name") as! String, object.objectForKey("openingDate") as! NSDate, object.objectForKey("address") as! String, object.objectForKey("neighborhood") as! String, object.objectForKey("description") as! String, object.objectForKey("foodType") as! String)
                 })
                 
                 var v: [Venue] = []
                 for (index, venue) in enumerate(fetchedVenues) {
-                    v.append(Venue(id: fetchedVenues[index].venueId, name: fetchedVenues[index].venueName, address: fetchedVenues[index].venueAddress, neighborhood: fetchedVenues[index].venueNeighborhood, description: fetchedVenues[index].venueDescription, foodType: fetchedVenues[index].venueFoodType))
+                    v.append(Venue(id: fetchedVenues[index].venueId, name: fetchedVenues[index].venueName, openingDate: fetchedVenues[index].venueOpeningDate, address: fetchedVenues[index].venueAddress, neighborhood: fetchedVenues[index].venueNeighborhood, description: fetchedVenues[index].venueDescription, foodType: fetchedVenues[index].venueFoodType))
                 }
                 callback(v)
             }
